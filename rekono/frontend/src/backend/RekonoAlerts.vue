452a012eecdef66f6fc25251effdf3ffe4299108
Ghost_Shell <91254737+Ghost00Shell@users.noreply.github.com>
2024-01-17 19:02:46 +0200

    Add files via upload

---
+++ rekono/frontend/src/backend/RekonoAlerts.vue
@@ -0,0 +1,20 @@
+
+<script>
+export default {
+  name: 'rekonoAlerts',
+  methods: {
+    alert (title, body, variant) {
+      this.$bvToast.toast(body, { title: title, variant: variant, solid: true })
+    },
+    success (title, body) {
+      this.alert(title, body, 'success')
+    },
+    warning (title, body) {
+      this.alert(title, body, 'warning')
+    },
+    danger (title, body) {
+      this.alert(title, body, 'danger')
+    }
+  }
+}
+</script>
